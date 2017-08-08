d24cf2a8968f171d45f00e7defb181505ffac371
Ryan Short <rcshort9376@hotmail.com>
2017-08-08 12:18:04 +1000

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("user flipped " + cardFour);
+console.log("user flipped " + cardOne);
