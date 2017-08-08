97e131fed0a9f6b5d8941bd10348db5712b8581c
Ryan Short <rcshort9376@hotmail.com>
2017-08-08 13:43:04 +1000

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,8 +1,24 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user flipped " + cardFour);
-console.log("user flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+  cardsInPlay.push("queen");
+var cardTwo = cards[3];
+  cardsInPlay.push("king");
+
+if (cardsInPlay.length === 2) {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!")
+  } else if (cardsInPlay[0] !== cardsInPlay[1]) {
+    alert("Sorry try again")
+  }
+
+}
+
+
+
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
